fcac02611806e07d165ae1e32d3ce0cd4f48bdf4
TomK <tkarnik@live.com>
2016-09-21 10:29:40 -0400

    Initial Awwa Master branch created.

---
+++ AwwaWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AwwaWeb.Startup))]
+namespace AwwaWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
